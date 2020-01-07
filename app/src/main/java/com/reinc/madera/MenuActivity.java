9b9ce4cf9798743d27f143c744c253861d9c0bac
Cynthia reinhardt <rein.cynthia@gmail.com>
2020-01-07 14:56:52 +0100

    add button to menu

--- app/src/main/java/com/reinc/madera/MenuActivity.java
+++ app/src/main/java/com/reinc/madera/MenuActivity.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
-    Button btnProjets, btnClients, btnDevis;
+    Button btnProjets, btnClients, btnDevis, newProject, projects, clients, newClient, devis;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,41 +18,77 @@ public class MenuActivity extends AppCompatActivity {
         btnProjets = (Button) findViewById(R.id.projets);
         btnClients = (Button) findViewById(R.id.clients);
         btnDevis = (Button) findViewById(R.id.devis);
-
+        newProject  = (Button)findViewById(R.id.newProjet);
+        projects = (Button)findViewById(R.id.voirProjet);
+        newClient  = (Button)findViewById(R.id.newClient);
+        clients = (Button)findViewById(R.id.voirClient);
+        devis = (Button)findViewById(R.id.voirDevis);
 
 
         btnClients.setOnClickListener(new View.OnClickListener() {
-
                                        public void onClick(View v) {
-
-                                           Intent intent = new Intent(MenuActivity.this, Client.class);
-                                           startActivity(intent);
-
-
+                                           newClient.setVisibility(View.VISIBLE);
+                                           clients.setVisibility(View.VISIBLE);
+                                           newProject.setVisibility(View.INVISIBLE);
+                                           projects.setVisibility(View.INVISIBLE);
+                                           devis.setVisibility(View.INVISIBLE);
                                        }
                                    }
         );
         btnProjets.setOnClickListener(new View.OnClickListener() {
-
                                          public void onClick(View v) {
-
-                                             Intent intent = new Intent(MenuActivity.this, Projet.class);
-                                             startActivity(intent);
-
-
+                                             newProject.setVisibility(View.VISIBLE);
+                                             projects.setVisibility(View.VISIBLE);
+                                             newClient.setVisibility(View.INVISIBLE);
+                                             clients.setVisibility(View.INVISIBLE);
+                                             devis.setVisibility(View.INVISIBLE);
                                          }
                                      }
         );
         btnDevis.setOnClickListener(new View.OnClickListener() {
-
                                           public void onClick(View v) {
-
-                                              Intent intent = new Intent(MenuActivity.this, Devis.class);
-                                              startActivity(intent);
-
-
+                                              newProject.setVisibility(View.INVISIBLE);
+                                              projects.setVisibility(View.INVISIBLE);
+                                              newClient.setVisibility(View.INVISIBLE);
+                                              clients.setVisibility(View.INVISIBLE);
+                                              devis.setVisibility(View.VISIBLE);
                                           }
                                       }
+        );
+        newProject.setOnClickListener(new View.OnClickListener() {
+                                        public void onClick(View v) {
+                                            Intent intent = new Intent(MenuActivity.this, Projet.class);
+                                            startActivity(intent);
+                                        }
+                                    }
+        );
+        projects.setOnClickListener(new View.OnClickListener() {
+                                        public void onClick(View v) {
+                                            Intent intent = new Intent(MenuActivity.this, ProjectsList.class);
+                                            startActivity(intent);
+                                        }
+                                    }
+        );
+        newClient.setOnClickListener(new View.OnClickListener() {
+                                          public void onClick(View v) {
+                                              Intent intent = new Intent(MenuActivity.this, Client.class);
+                                              startActivity(intent);
+                                          }
+                                      }
+        );
+        clients.setOnClickListener(new View.OnClickListener() {
+                                        public void onClick(View v) {
+                                            Intent intent = new Intent(MenuActivity.this, Client.class);
+                                            startActivity(intent);
+                                        }
+                                    }
+        );
+        devis.setOnClickListener(new View.OnClickListener() {
+                                       public void onClick(View v) {
+                                           Intent intent = new Intent(MenuActivity.this, Devis.class);
+                                           startActivity(intent);
+                                       }
+                                   }
         );
     }
 }
